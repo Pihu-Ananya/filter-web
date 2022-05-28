1aed966482dee6b00d2cf3ec5a3f12a2c4947d69
Pihu-Ananya <97041050+Pihu-Ananya@users.noreply.github.com>
2022-05-28 12:03:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -10,15 +10,32 @@ function  setup() {
     video=createCapture(VIDEO)
     video.size(400,400)
     video.hide()
-
+    posenet=ml5.poseNet(video,modelloaded)
+    posenet.on("pose" , gotposes)
 }
 
-function  draw()  {
+
+  function modelloaded() {
+    console.log("posenet is initialised");
+  }
+  
+function gotposes(Results) {
+  if ( Results.length > 0) {
+      console.log(Results)
+      nose_x=Results[0].pose.nose.x
+      nose_y=Results[0].pose.nose.y
+      console.log("nosex=" , nose_x);
+      console.log("nosey=" , nose_y);
+  }
+}
+
+  function  draw()  {
     image(video,0,0,400,400)
     fill(255 , 0 , 0);
     stroke(255 , 0 , 0);
-    image(lipstick , nose_x-20 , nose_y-20 , 30, 30)
+    image(lipstick , nose_x-30 , nose_y+20 , 30, 30)
   }
+    
 
 
   function takeSnapshot() {
